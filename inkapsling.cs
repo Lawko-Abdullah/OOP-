f4d2a79968a4986d4f73b55e828056cb2c2a56c9
Lawko-Abdullah <lawkoabdulla02@gmail.com>
2024-09-15 18:33:57 +0200

    Update inkapsling.cs

--- inkapsling.cs
+++ inkapsling.cs
@@ -4,25 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exempel
+    namespace Exempel
 
-{
-    class Person
     {
-        // Privat string för namn (kan bara nås inom klassen)
-        private string name;
-
-        // Publik metod för att sätta värdet  'name'
-        public void SetName(string value)
+        class Person
         {
-            name = value;
+            // Privat string för namn (kan bara nås inom klassen)
+            private string name;
+
+            // Publik metod för att sätta värdet  'name'
+            public void SetName(string value)
+            {
+                name = value;
+            }
+
+            // Publik metod för att hämta värdet  'name'
+            public string GetName()
+            {
+                return name;
+            }
+
+            
         }
 
-        // Publik metod för att hämta värdet  'name'
-        public string GetName()
-        {
-            return name;
-        }
-
-
-}
+    }
